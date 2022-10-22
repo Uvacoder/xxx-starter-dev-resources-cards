2de2036ad98a034df9ee9f6adf49c2b9f6e2924e
Uvacoder <uva22940coder@gmail.com>
2022-10-22 16:31:38 -0400

    Update sources.js

--- src/data/sources.js
+++ src/data/sources.js
@@ -1,7 +1,8 @@
-import { BsStack, BsCodeSlash, BsFillBookmarkHeartFill } from "react-icons/bs";
+import { BsStack, BsCodeSlash, BsFillBookmarkHeartFill, BsPersonSquare } from "react-icons/bs";
 
 export const sources = [
   { name: "Animation", Icon: BsStack },
   { name: "Code", Icon: BsCodeSlash },
   { name: "Bookmarks", Icon: BsFillBookmarkHeartFill },
+  { name: "Portfolios", Icon: BsPersonSquare },
 ];
